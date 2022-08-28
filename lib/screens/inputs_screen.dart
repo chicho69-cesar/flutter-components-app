a21ef5b3da986e3a63d211d83c9d95009715f3fd
Cesar <cesarvillalobosolmos.01@gmail.com>
2022-08-28 13:59:43 -0500

    Components app: Inputs screens started

---
+++ lib/screens/inputs_screen.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class InputsScreen extends StatelessWidget {
+  const InputsScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Center(
+          child: Text(
+            'Inputs y Forms',
+            style: TextStyle(fontSize: 35),
+          ),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+          child: Column(
+            children: [
+              TextFormField(
+                autofocus: true,
+                initialValue: 'Cesar Villalobos Olmos',
+                textCapitalization: TextCapitalization.words,
+                style: const TextStyle(fontSize: 25),
+                onChanged: (value) {
+                  print('Value: $value');
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
