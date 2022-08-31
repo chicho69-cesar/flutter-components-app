5d37fb7b0925095475164fb958ec6b8c9edc6640
Cesar <cesarvillalobosolmos.01@gmail.com>
2022-08-31 17:21:24 -0500

    Components app: Sliders and Checks screens started

--- lib/router/app_routes.dart
+++ lib/router/app_routes.dart
@@ -15,6 +15,7 @@ class AppRoutes {
     MenuOption(route: 'avatar', name: 'Circle Avatar', screen: const AvatarScreen(), icon: Icons.supervised_user_circle_outlined),
     MenuOption(route: 'animated', name: 'Animated Container', screen: const AnimatedScreen(), icon: Icons.play_circle_fill_outlined),
     MenuOption(route: 'inputs', name: 'Text Inputs', screen: const InputsScreen(), icon: Icons.input_rounded),
+    MenuOption(route: 'slider', name: 'Sliders and Checks', screen: const SliderScreen(), icon: Icons.slow_motion_video),
   ];
 
   static Map<String, Widget Function(BuildContext)> getAppRoutes() {
